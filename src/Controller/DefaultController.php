2098c26b84e52c8b0fa8005b61144dd21998518f
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2019-07-10 14:12:37 +0200

    Displayed README.md on homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,11 @@ class DefaultController extends AbstractController
     /**
      * @Route("/{name}", name="homepage")
      */
-    public function index(?string $name = null)
+    public function index(string $projectDir, MarkdownParserInterface $parser, ?string $name = null)
     {
         return $this->render('default/index.html.twig', [
             'name' => $name,
+            'description' => $parser->transformMarkdown(file_get_contents($projectDir . DIRECTORY_SEPARATOR . 'README.md'))
         ]);
     }
 }
