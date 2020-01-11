de273a9177bdd7295d339b9d626d47b59fa8a89c
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-01-11 13:34:50 +0100

    :art: Added PHP CS

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of Silarhi.
+ * (c) Guillaume Sainthillier <hello@silarhi.fr>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace App\Controller;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
@@ -14,7 +22,7 @@ class DefaultController extends AbstractController
     public function index(string $projectDir, MarkdownParserInterface $parser)
     {
         return $this->render('default/index.html.twig', [
-            'description' => $parser->transformMarkdown(file_get_contents($projectDir . DIRECTORY_SEPARATOR . 'README.md'))
+            'description' => $parser->transformMarkdown(file_get_contents($projectDir . \DIRECTORY_SEPARATOR . 'README.md')),
         ]);
     }
 }
