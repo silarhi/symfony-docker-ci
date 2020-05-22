2b3273f0d50db3820777934fa1b87be07f32520b
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-05-22 22:21:30 +0200

    :truck: Splitted assets entrypoints to prepare new pages

--- webpack.config.js
+++ webpack.config.js
@@ -18,11 +18,10 @@ Encore
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
-    //.addEntry('page2', './assets/js/page2.js')
+    .addEntry('index', './assets/js/pages/index.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    //.splitEntryChunks()
+    .splitEntryChunks()
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
