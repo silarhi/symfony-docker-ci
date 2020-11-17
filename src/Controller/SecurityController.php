b023bcb52f5265fb9b8efe166b7d7c3c84bb8648
Florian Mandile <55392546+FlorianM11@users.noreply.github.com>
2020-11-17 11:28:26 +0100

    Features/2fa (#8)
    * WIP
    
    * Update design
    
    * Actually check user password
    
    * Improved login form for demo purpose
    
    * Fixed in_memory provider
    
    * Update
    
    * Last Update
    
    * Change language and other
    
    * Change ONE word
    
    * Last change
    
    * Change language french at english
    
    Co-authored-by: Guillaume Sainthillier <guillaume.sainthillier@gmail.com>

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * This file is part of SILARHI.
+ * (c) 2019 - 2020 Guillaume Sainthillier <hello@silarhi.fr>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace App\Controller;
+
+use BaconQrCode\Renderer\Image\ImagickImageBackEnd;
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
+use PragmaRX\Google2FA\Google2FA;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/authentification", name="app_security_authentification")
+     */
+    public function index(SessionInterface $session)
+    {
+        if (!$session->has('qrCodeSession')) {
+            $google2fa = new Google2FA();
+
+            $secretKey = $google2fa->generateSecretKey();
+
+            $qrCodeUrl = $google2fa->getQRCodeUrl(
+                'Silarhi',
+                'hello@silarhi.fr',
+                $secretKey
+            );
+
+            $writer = new Writer(
+                new ImageRenderer(
+                    new RendererStyle(400),
+                    new ImagickImageBackEnd()
+                )
+            );
+
+            $qrCodeImage = base64_encode($writer->writeString($qrCodeUrl));
+
+            $qrCodeSession = [
+                'secretKey' => $secretKey,
+                'qrCodeImage' => $qrCodeImage,
+            ];
+
+            $session->set('qrCodeSession', $qrCodeSession);
+        }
+
+        $getQrCodeSession = $session->get('qrCodeSession');
+
+        return $this->render('security/qrCode.html.twig', [
+            'qrCodeImage' => $getQrCodeSession['qrCodeImage'],
+            'secretKey' => $getQrCodeSession['secretKey'],
+        ]);
+    }
+
+    /**
+     * @Route("/valide-authentification", name="app_security_validate_authentification")
+     */
+    public function valideAuthentification(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('security/valide_authentification.html.twig', [
+            'error' => $error,
+        ]);
+    }
+
+    /**
+     * @Route("/2FA-protected", name="app_security_authentification_protected")
+     */
+    public function authentificationProtected()
+    {
+        return $this->render('security/protected.html.twig');
+    }
+}
---
+++ src/Security/QRCodeAuthenticator.php
@@ -0,0 +1,127 @@
+<?php
+
+/*
+ * This file is part of SILARHI.
+ * (c) 2019 - 2020 Guillaume Sainthillier <hello@silarhi.fr>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace App\Security;
+
+use App\EventSuscriber\DoubleAuthentificationSuscriber;
+use PragmaRX\Google2FA\Google2FA;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class QRCodeAuthenticator extends AbstractFormLoginAuthenticator
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_security_validate_authentification';
+
+    /** @var UrlGeneratorInterface */
+    private $urlGenerator;
+
+    /** @var CsrfTokenManagerInterface */
+    private $csrfTokenManager;
+
+    /** @var UserPasswordEncoderInterface */
+    private $passwordEncoder;
+
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
+
+    /** @var SessionInterface */
+    private $session;
+
+    public function __construct(SessionInterface $session, TokenStorageInterface $tokenStorage, UrlGeneratorInterface $urlGenerator, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->tokenStorage = $tokenStorage;
+        $this->session = $session;
+        $this->urlGenerator = $urlGenerator;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function supports(Request $request)
+    {
+        return self::LOGIN_ROUTE === $request->attributes->get('_route')
+            && $request->isMethod('POST');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        return [
+            'qrCode' => $request->request->get('qrCode'),
+        ];
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        //Get user from login form
+        $existingToken = $this->tokenStorage->getToken();
+        if (null === $existingToken) {
+            return null;
+        }
+
+        return $this->tokenStorage->getToken()->getUser();
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        $qrCode = (string) $credentials['qrCode'];
+
+        if (!$user) {
+            return false;
+        }
+
+        $google2fa = new Google2FA();
+        $google2fa->setSecret($this->session->get('qrCodeSession')['secretKey']);
+
+        if (true !== $google2fa->verifyKey($google2fa->getSecret(), $qrCode)) {
+            throw new CustomUserMessageAuthenticationException("le code n'est pas valide");
+        }
+
+        return true;
+    }
+
+    public function createAuthenticatedToken(UserInterface $user, string $providerKey)
+    {
+        $currentToken = parent::createAuthenticatedToken($user, $providerKey);
+
+        $roles = array_merge($currentToken->getRoleNames(), [DoubleAuthentificationSuscriber::ROLE_2FA_SUCCEED]);
+
+        return new PostAuthenticationGuardToken(
+            $currentToken->getUser(),
+            $currentToken->getProviderKey(),
+            $roles
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate('app_security_authentification_protected'));
+    }
+
+    protected function getLoginUrl()
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+}
