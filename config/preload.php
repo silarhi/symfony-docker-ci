9ce8307daa914860dc3d0134cee25fdc672e0ece
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2020-11-17 19:48:29 +0100

    :sparkles: Re apply symfony recipes to mirror best practices

---
+++ config/preload.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This file is part of SILARHI.
+ * (c) 2019 - 2020 Guillaume Sainthillier <hello@silarhi.fr>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+if (file_exists(\dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php')) {
+    require \dirname(__DIR__) . '/var/cache/prod/App_KernelProdContainer.preload.php';
+}
