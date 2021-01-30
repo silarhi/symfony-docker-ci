ed10c3948bea03c01aea31c6466e5d6f98da4a40
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2021-01-30 12:22:00 +0100

    Use traefik as  trusted proxy

--- public/index.php
+++ public/index.php
@@ -29,7 +29,7 @@ if ($_SERVER['APP_DEBUG']) {
 }
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_TRAEFIK);
 }
 
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
