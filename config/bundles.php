9bcd53db1d55880c62e48e627af9653b4b82f86b
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2022-01-04 18:31:31 +0100

    Used markdown with twig instead of knp bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,7 +16,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,17 +10,16 @@
 
 namespace App\Controller;
 
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route(path: '/', name: 'homepage')]
-    public function index(string $projectDir, MarkdownParserInterface $parser)
+    public function index(string $projectDir)
     {
         return $this->render('default/index.html.twig', [
-            'description' => $parser->transformMarkdown(file_get_contents($projectDir . \DIRECTORY_SEPARATOR . 'README.md')),
+            'description' => file_get_contents($projectDir . \DIRECTORY_SEPARATOR . 'README.md'),
         ]);
     }
 }
