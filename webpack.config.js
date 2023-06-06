47fbd41279bc11c6d6f6ecde7d24e11003905faf
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2023-06-06 09:29:47 +0200

    disable integrity as cloudflare change files

--- webpack.config.js
+++ webpack.config.js
@@ -71,11 +71,11 @@ Encore
     //.enableTypeScriptLoader()
 
     // uncomment if you use React
-    .enableReactPreset()
+    .enableReactPreset();
 
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    .enableIntegrityHashes(Encore.isProduction());
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+// .enableIntegrityHashes(Encore.isProduction())
 
 // uncomment if you're having problems with a jQuery plugin
 //.autoProvidejQuery()
