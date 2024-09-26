fbbf4ba3d9d4ff9351de218da07e8847ae1318be
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2024-09-26 16:28:47 +0200

    prevent react mounting during turbo preview

--- assets/js/pages/index.jsx
+++ assets/js/pages/index.jsx
@@ -6,12 +6,15 @@ import {createRoot} from 'react-dom/client';
 class IndexController extends Controller {
     #root;
     connect() {
+        if(document.documentElement.hasAttribute("data-turbo-preview")) {
+            return;
+        }
         this.#root = createRoot(this.element);
         this.#root.render(<App/>);
     }
 
     disconnect() {
-        this.#root.unmount();
+        this.#root?.unmount();
     }
 }
 
