7c231c14a43599dcbac2352fdff4f60d881ba091
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2024-12-26 16:36:00 +0100

    upgrade to phpstan 2

--- src/Form/ActivationDoubleAuthentificationType.php
+++ src/Form/ActivationDoubleAuthentificationType.php
@@ -15,6 +15,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
+/**
+ * @extends AbstractType<mixed>
+ */
 class ActivationDoubleAuthentificationType extends AbstractType
 {
     #[Override]
