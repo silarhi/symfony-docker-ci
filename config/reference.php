7b32ec6e39f865b0e5c1766928279cd7d0647e61
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2026-04-20 15:43:51 +0200

    upgrade packages

--- config/reference.php
+++ config/reference.php
@@ -1259,7 +1259,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *     default_transport?: scalar|Param|null, // Default: "default"
  * }
  * @psalm-type TwigComponentConfig = array{
- *     defaults?: array<string, string|array{ // Default: ["__deprecated__use_old_naming_behavior"]
+ *     defaults?: array<string, string|array{ // Default: []
  *         template_directory?: scalar|Param|null, // Default: "components"
  *         name_prefix?: scalar|Param|null, // Default: ""
  *     }>,
@@ -1268,7 +1268,6 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         enabled?: bool|Param, // Default: "%kernel.debug%"
  *         collect_components?: bool|Param, // Collect components instances // Default: true
  *     },
- *     controllers_json?: scalar|Param|null, // Deprecated: The "twig_component.controllers_json" config option is deprecated, and will be removed in 3.0. // Default: null
  * }
  * @psalm-type PicassoConfig = array{
  *     default_loader?: scalar|Param|null, // Default loader name. // Default: null
